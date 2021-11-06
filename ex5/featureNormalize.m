8710bb5bb20c98b1b0799956ef754a3befa42413
Duc Anh Ho <92146886+ducanhho2296@users.noreply.github.com>
2021-11-06 10:34:21 +0100

    Update featureNormalize.m

--- ex5/featureNormalize.m
+++ ex5/featureNormalize.m
@@ -1,2 +1,8 @@
+%FEATURENORMALIZE Normalizes the features in X 
+%   FEATURENORMALIZE(X) returns a normalized version of X where
+%   the mean value of each feature is 0 and the standard deviation
+%   is 1. This is often a good preprocessing step to do when
+%   working with learning algorithms.
 
 
+% ============================================================
