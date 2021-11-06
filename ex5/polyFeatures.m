3f0f5eb3e1175b828bf33181658ec78ecef2562b
Duc Anh Ho <92146886+ducanhho2296@users.noreply.github.com>
2021-11-06 10:35:44 +0100

    Update polyFeatures.m

--- ex5/polyFeatures.m
+++ ex5/polyFeatures.m
@@ -1,19 +1,11 @@
 function [X_poly] = polyFeatures(X, p)
 
-
-
-% You need to return the following variables correctly.
 X_poly = zeros(numel(X), p);
-
-
 X_poly(:,1) = X(:,1);
 
 for i = 2:p
   X_poly(:,i) = X(:,1).^i;
 endfor;
-
-
-
 % =========================================================================
 
 end
