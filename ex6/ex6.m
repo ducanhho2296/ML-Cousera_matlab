d449ed89dd57ae79fb9626724cb6c93bfa646b35
Duc Anh Ho <92146886+ducanhho2296@users.noreply.github.com>
2021-11-13 09:40:38 +0100

    Update ex6.m

--- ex6/ex6.m
+++ ex6/ex6.m
@@ -1,3 +1,30 @@
+%% Machine Learning Online Class
+%  Exercise 6 | Support Vector Machines
+%
+%  Instructions
+%  ------------
+% 
+%  This file contains code that helps you get started on the
+%  exercise. You will need to complete the following functions:
+%
+%     gaussianKernel.m
+%     dataset3Params.m
+%     processEmail.m
+%     emailFeatures.m
+%
+%  For this exercise, you will not need to change any code in this file,
+%  or any other files other than those mentioned above.
+%
+
+%% Initialization
+clear ; close all; clc
+
+%% =============== Part 1: Loading and Visualizing Data ================
+%  We start the exercise by first loading and visualizing the dataset. 
+%  The following code will load the dataset into your environment and plot
+%  the data.
+%
+
 fprintf('Loading and Visualizing Data ...\n')
 
 % Load from ex6data1: 
