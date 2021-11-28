dd3f68eb7459d63bc81ba63809fb0f5822d4e3ec
Duc Anh Ho <92146886+ducanhho2296@users.noreply.github.com>
2021-11-28 09:39:30 +0100

    Create selectThreshold.m

---
+++ ex8/selectThreshold.m
@@ -0,0 +1,30 @@
+function [bestEpsilon bestF1] = selectThreshold(yval, pval)
+bestEpsilon = 0;
+bestF1 = 0;
+F1 = 0;
+
+stepsize = (max(pval) - min(pval)) / 1000;
+for epsilon = min(pval):stepsize:max(pval)
+    
+    predictions = (pval < epsilon);
+    pred_anomaly = sum(predictions);       % = t_p + f_p
+    
+    false_positive = sum((predictions == 1) & (yval == 0)); %f_p
+    true_positive = pred_anomaly - false_positive;           %t_p
+    false_negative = sum((predictions == 0) & (yval == 1)); %f_n
+
+    %compute Precision and recall
+    prec = true_positive / (true_positive + false_positive);
+    rec = true_positive / (true_positive + false_negative);
+
+    % calculate F1
+    F1 = (2 * prec * rec) / (prec + rec);
+    % =============================================================
+
+    if F1 > bestF1
+       bestF1 = F1;
+       bestEpsilon = epsilon;
+    end
+end
+
+end
