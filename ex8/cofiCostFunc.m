a79fc8b745748cc9e7af55ba7db497d8a18740f8
Duc Anh Ho <92146886+ducanhho2296@users.noreply.github.com>
2021-12-08 20:11:10 +0100

    Update cofiCostFunc.m

--- ex8/cofiCostFunc.m
+++ ex8/cofiCostFunc.m
@@ -1,4 +1,9 @@
 function [J, grad] = cofiCostFunc(params, Y, R, num_users, num_movies, ...
+                                  num_features, lambda)
+%COFICOSTFUNC Collaborative filtering cost function
+%   [J, grad] = COFICOSTFUNC(params, Y, R, num_users, num_movies, ...
+%   num_features, lambda) returns the cost and gradient for the
+%   collaborative filtering problem.
 %
 
 % Unfold the U and W matrices from params
